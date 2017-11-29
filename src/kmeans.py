35705e204fe55b1b3ac8e22a57efe79293e0f769
Matteo Cassia <matteo.cassia@me.com>
2017-11-29 10:16:06 +0000

    Added file for mapping user reputation and the total number of comments posted. (#5)
    * Added mapping file for User Reputation and number of comments.
    
    * Moved file to example/
    
    * Updated KMeans class to embed n-dimensional points with relative unit tests.
    
    * Fixed issue about radomly generated centroids not being assigned to any point.
    
    * Fixed code to make it compliant to Python standards.

--- src/kmeans.py
+++ src/kmeans.py
@@ -8,12 +8,12 @@ class KMeans:
         """Initialises a KMeans classifier with the list of points and the target number of clusters"""
         self.points = points
         self.k = clusters
+        self.n = len(points[0])
         self.outer_vertices = self.find_outer_vertices()
 
     def find_cluster_centroids(self):
         """Runs an infinite loop which contains an iteration on the self.points to find the nearest mean"""
         centroids = self.generate_random_centroids()
-
         for i in range(100):
             clusters = {}
 
@@ -32,8 +32,10 @@ class KMeans:
 
                 clusters[best_centroid].append(point)
 
-            centroids = [self.calculate_centroid(points) for points in clusters.values()]
-
+            new_centroids = [self.calculate_centroid(points) for points in clusters.values()]
+            for j in range(0, len(centroid) - len(new_centroids)):
+                new_centroids.append(self.generate_random_point())
+            centroids = new_centroids
         return centroids
 
     def generate_random_centroids(self):
@@ -42,31 +44,34 @@ class KMeans:
 
     @staticmethod
     def calculate_centroid(points):
-        x = [p[0] for p in points]
-        y = [p[1] for p in points]
-        return numpy.mean(x), numpy.mean(y)
+        n = len(points[0])
+        means = []
+        for i in range(0, n):
+            means.append(numpy.mean([p[i] for p in points]))
+        return tuple(means)
 
     @staticmethod
     def calculate_distance(a, b):
         """Returns the distance between two points"""
-        distance_x = abs(a[0] - b[0])
-        distance_y = abs(a[1] - b[1])
-        return math.sqrt(pow(distance_x, 2) + pow(distance_y, 2))
+        deltas = map(lambda p: pow(p[0] - p[1], 2), zip(a, b))
+        return math.sqrt(sum(deltas))
 
     def generate_random_point(self):
         """Returns a random point within the edges provided"""
-        x = random.uniform(self.outer_vertices[0], self.outer_vertices[1])
-        y = random.uniform(self.outer_vertices[2], self.outer_vertices[3])
-        return x, y
+        coordinates = []
+        for i in range(self.n):
+            coordinates.append(random.uniform(self.outer_vertices[2 * i], self.outer_vertices[2 * i + 1]))
+        return tuple(coordinates)
 
     def find_outer_vertices(self):
         """Gets the edges of the point set"""
-        edges = [
-            float("+inf"), float("-inf"), float("+inf"), float("-inf"),
-        ]
+        edges = []
+        # sets the initial edges to be +inf and -inf for every dimension
+        for i in range(0, self.n):
+            edges.append(float("+inf"))
+            edges.append(float("-inf"))
         for point in self.points:
-            edges[0] = min(edges[0], point[0])
-            edges[1] = max(edges[1], point[0])
-            edges[2] = min(edges[2], point[1])
-            edges[3] = max(edges[3], point[1])
+            for i in range(0, self.n):
+                edges[2 * i] = min(edges[2 * i], point[i])
+                edges[2 * i + 1] = max(edges[2 * i + 1], point[i])
         return tuple(edges)
