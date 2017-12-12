5fdcdde15489206c15830ce19e8faa0ea3e18fcf
Marcel Tyszkiewicz <marcel@flypay.co.uk>
2017-12-12 21:27:13 +0000

    Matteo + Cameron jobs

--- src/spark_jobs.py
+++ src/spark_jobs.py
@@ -143,3 +143,119 @@ def user__upvotes_cast__to__average_post_length__to__profile_views(k, users, pos
         .map(lambda data: (data[1][0][0], data[1][0][1], data[1][1]))
 
     return KMeans(k).fit(joined_data)
+
+
+def user__badges__to__signup__to__answers_and_questions(k, user_lines, badges_lines, posts_lines):
+    # (user_id, signup)
+    user_id_signup = user_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['Id', 'CreationDate'])) \
+        .filter(lambda a: any(a)) \
+        .map(lambda a: (int(a[0]), datetime_to_timestamp(a[1])))
+
+    # (user_id, number_badges)
+    user_id_badges = badges_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['UserId'], int)) \
+        .filter(lambda a: any(a)) \
+        .map(lambda a: (a[0], 1)) \
+        .reduceByKey(add)
+
+    # (user_id, post_type)
+    posts = posts_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['OwnerUserId', 'PostTypeId'], int)) \
+        .filter(lambda a: any(a)) \
+ 
+        # (user_id, n_answers)
+    user_id_answers = posts \
+        .filter(lambda a: a[1] == 2) \
+        .map(lambda a: (a[0], 1)) \
+        .reduceByKey(add)
+
+    # (user_id, n_asked)
+    user_id_questions = posts \
+        .filter(lambda a: a[1] == 1) \
+        .map(lambda a: (a[0], 1)) \
+        .reduceByKey(add)
+
+    # (n_questions, n_answers, n_badges, signup)
+    result = user_id_signup.join(user_id_badges)
+    result = result.join(user_id_answers)
+    result = result.join(user_id_questions)
+    result = result.map(lambda a: (a[1][1], a[1][0][1], a[1][0][0][1], a[1][0][0][0]))
+
+    return KMeans(k).fit(result)
+
+
+def user__reputation__to__own_questions_answered(k, user_lines, post_lines):
+    # (user_id, rep)
+    user_id_reputation = user_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['Id', 'Reputation'], int)) \
+        .filter(lambda a: any(a))
+
+    # (user_id, n_questions)
+    user_id_questions = post_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['Id', 'OwnerUserId', 'PostTypeId'], int)) \
+        .filter(lambda a: None not in a and len(a) == 3 and a[2] == 1) \
+        .map(lambda a: (a[0], a[1]))
+
+    # (user_id, n_asked)
+    user_id_answers = post_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['ParentId', 'OwnerUserId', 'PostTypeId'], int)) \
+        .filter(lambda a: None not in a and len(a) == 3 and a[2] == 2) \
+        .map(lambda a: (a[0], a[1]))
+
+    # (user_id, n_questions_self_answered)
+    user_id_own_answers = user_id_questions.intersection(user_id_answers) \
+        .map(lambda a: (a[1], 1)) \
+        .reduceByKey(add)
+
+    # (rep, n_questions_self_answered)
+    result = user_id_reputation.join(user_id_own_answers).map(lambda a: (a[1][0], a[1][1]))
+
+    return KMeans(k).fit(result)
+
+
+def user__signup__to__distinct_post_tags(k, user_lines, post_lines):
+    # (user_id, signup_as_timestamp)
+    creationdate = user_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['Id', 'CreationDate'], str)) \
+        .filter(lambda a: any(a)) \
+        .map(lambda a: (int(a[0]), datetime_to_timestamp(a[1])))
+
+    # (user_id, number_distinct_tags)
+    user_id_tags = post_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['OwnerUserId', 'Tags'], str)) \
+        .filter(lambda a: (None not in a and len(a) == 2) \
+        .map(lambda a: (int(a[0]), a[1].replace(">", "")[1:])) \
+        .map(lambda a: (a[0], a[1].split("<"))) \
+        .flatMapValues(lambda a: a) \
+        .distinct() \
+        .map(lambda a: (a[0], 1)) \
+        .reduceByKey(sum)
+
+    # (signup_as_timestamp, number_distinct_tags)
+    result = creationdate.join(user_id_tags).map(lambda a: (a[1][0], a[1][1]))
+
+    return KMeans(k).fit(result)
+
+
+def user__reputation__to__distinct_post_tags(k, user_lines, post_lines):
+    # (user_id, reputation)
+    rep = user_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['Id', 'Reputation'], int)) \
+        .filter(lambda a: any(a))
+
+    # (user_id, number_distinct_tags)
+    user_id_tags = post_lines \
+        .map(lambda line: xml_parser.extract_attributes(line, ['OwnerUserId', 'Tags'])) \
+        .filter(lambda a: (None not in a and len(a) == 2)) \
+        .map(lambda a: (int(a[0]), a[1].replace(">", "")[1:])) \
+        .map(lambda a: (a[0], a[1].split("<"))) \
+        .flatMapValues(lambda a: a) \
+        .distinct() \
+        .map(lambda a: (a[0], 1)) \
+        .reduceByKey(sum)
+
+    # (rep, number_distinct_tags)
+    result = rep.join(user_id_tags).map(lambda a: (a[1][0], a[1][1]))
+
+    return KMeans(k).fit(result)
