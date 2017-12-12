90466737092de91375a5e574847841cf5c50ebe0
Marcel Tyszkiewicz <marcel@flypay.co.uk>
2017-12-12 21:43:45 +0000

    fix xml parsing

--- src/xml_parser.py
+++ src/xml_parser.py
@@ -1,6 +1,23 @@
 import xml.etree.ElementTree as ET
 
 
+def extract_attribute_value(xml, attribute_name):
+    beg = '%s="' % attribute_name
+    end = '" '
+
+    start_index = xml.find(beg)
+    if start_index == -1:
+        return None
+
+    substring = xml[start_index + len(beg):len(xml)]
+
+    end_index = substring.find(end)
+    if end_index == -1:
+        return None
+
+    return substring[0:end_index]
+
+
 def extract_attributes(xml, attribute_names, cast=str):
     """
     Extracts a set of attribute values from an XML line.
@@ -10,33 +27,11 @@ def extract_attributes(xml, attribute_names, cast=str):
     :param cast: callback to apply to each value
     :return: tuple of attribute values
     """
-    data = parse_xml_line(xml)
     values = []
 
-    if data is None:
-        return tuple(values)
-
     for attribute_name in attribute_names:
-        if attribute_name in data.attrib:
-            value = data.attrib[attribute_name].strip()
-            values.append(cast(value))
-        else:
-            values.append(None)
+        value = extract_attribute_value(xml, attribute_name)
+        if value:
+            values.append(cast(value.encode('utf-8').strip()))
 
     return tuple(values)
-
-
-def parse_xml_line(line):
-    try:
-        line = cleanup_xml(line.encode('utf-8').strip())
-        return ET.fromstring(line)
-    except ET.ParseError as e:
-        # print(e, line.strip())
-        return None
-
-
-def cleanup_xml(line):
-    replacements = {
-        '&^Cquot': '&quot',
-    }
-    return reduce(lambda x, y: x.replace(y, replacements[y]), replacements, line)
--- tests/xml_parser_test.py
+++ tests/xml_parser_test.py
@@ -3,19 +3,6 @@ import xml_parser
 
 
 class ParserTest(unittest.TestCase):
-    def test_parse_xml_line__correctly_parses_xml(self):
-        xml = '<user id="19" date="2009-01-12T19:07:38.647" name="Tassos" />'
-        data = xml_parser.parse_xml_line(xml)
-
-        self.assertEqual(data.tag, 'user')
-        self.assertEqual(data.attrib['id'], '19')
-        self.assertEqual(data.attrib['date'], '2009-01-12T19:07:38.647')
-        self.assertEqual(data.attrib['name'], 'Tassos')
-
-    def test_parse_xml_line__returns_none_when_xml_malformed(self):
-        xml = '<user>'
-        self.assertIsNone(xml_parser.parse_xml_line(xml))
-
     def test_extract_attributes(self):
         xml = '<user id="19" date="  2009-01-12T19:07:38.647  " name="Tassos  " />'
         values = xml_parser.extract_attributes(xml, ["date", "name"])
@@ -25,8 +12,3 @@ class ParserTest(unittest.TestCase):
         xml = '<user id="19" rep="90050" year="2017" />'
         values = xml_parser.extract_attributes(xml, ["id", "rep", "year"], int)
         self.assertEqual(values, (19, 90050, 2017))
-
-    def test_cleanup_xml(self):
-        before = '<p>lol&^Cquot</p>'
-        after = '<p>lol&quot</p>'
-        self.assertEqual(xml_parser.cleanup_xml(before), after)
